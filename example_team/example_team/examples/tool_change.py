c312a81083ff7bbcbf26366a98b9b987fb84a85e
Justin Albrecht <justin.albrecht@nist.gov>
2025-11-13 16:03:38 -0500

    adds tool change demo

---
+++ example_team/example_team/examples/tool_change.py
@@ -0,0 +1,49 @@
+import asyncio
+
+import rclpy
+from rclpy.executors import MultiThreadedExecutor
+
+from ariac_interfaces.msg import VacuumTools
+
+from example_team.environment_interface import Environment
+from example_team.robots import AssemblyRobot2
+from example_team.utils import AsyncUtils
+
+async def run():
+    rclpy.init()
+
+    # Create and spin executor
+    executor = MultiThreadedExecutor()
+    shutdown_event = asyncio.Event()
+    spin_task = asyncio.create_task(AsyncUtils.spin_executor(executor, shutdown_event))
+
+    environment = Environment()
+    executor.add_node(environment)
+
+    # Create robot nodes
+    ar2 = AssemblyRobot2()
+    executor.add_node(ar2)
+
+    try:
+        # Wait for competition to be ready
+        environment.get_logger().info("Waiting for environment to be ready")
+        await environment.competition.ready.wait()
+
+        await ar2.ready()
+
+        for i in range(10):
+            if i%2 == 0:
+                await ar2.pick_tool_from_stand(VacuumTools.VG_2)
+            else:
+                await ar2.pick_tool_from_stand(VacuumTools.VG_4)
+            
+            await ar2.return_tool_to_stand()
+
+    except Exception as e:
+        print(5*'\n', e, 5*'\n')
+    finally:
+        shutdown_event.set()
+        await spin_task  
+
+def main():
+    asyncio.run(run())
--- example_team/example_team/robots.py
+++ example_team/example_team/robots.py
@@ -1,10 +1,17 @@
+from typing import cast
+
 import math 
 
 import tf2_ros
 
+from rclpy.duration import Duration
+
 from tf_transformations import quaternion_from_euler
 
 from geometry_msgs.msg import Pose, Point, Vector3, Quaternion
+
+from ariac_interfaces.msg import VacuumTools
+from ariac_interfaces.srv import AttachTool, Trigger
 
 from example_team.robot_interface import Robot
 from example_team.gripper_interface import Gripper
@@ -75,3 +82,104 @@ class InspectionRobot2(Robot):
 
         plan = await self.plan_to_joint_state(self.joint_states['ready'], vsf=0.5)
         await self.execute(plan)
+
+
+class AssemblyRobot2(Robot):
+    def __init__(self):
+        super().__init__('assembly_robot_2')
+
+        self.tool_locations: dict[int, Vector3] = {}
+
+        self.orientations: dict[str, Quaternion] = {
+            'tool_changer': quaternion_from_list(quaternion_from_euler(math.pi, 0.0, math.pi)),
+        }
+
+        # Initialize TF
+        self.tf_buffer = tf2_ros.Buffer()
+        tf2_ros.TransformListener(self.tf_buffer, self)
+
+        self.attached_tool = VacuumTools.NONE
+
+        self.attach_tool_client = self.create_client(AttachTool, '/assembly_robot_2/tool_changer/attach_tool')
+        self.detach_tool_client = self.create_client(Trigger, '/assembly_robot_2/tool_changer/detach_tool')
+
+    async def ready(self): 
+        await super().ready()
+
+        # Fill tf locations
+        tool_frames = {
+            VacuumTools.VG_2: "tool_holder_vg_2",
+            VacuumTools.VG_4: "tool_holder_vg_4",
+        }
+
+        for tool, frame in tool_frames.items():
+            t = await AsyncUtils.get_tf_transform(self.tf_buffer, 'world', frame)
+            self.tool_locations[tool] = t.transform.translation
+
+    async def pick_tool_from_stand(self, tool_type: int):
+        if tool_type not in (VacuumTools.VG_2, VacuumTools.VG_4):
+            self.get_logger().warn(f"Unable to attach {tool_type}. Invalid tool type")
+            return
+
+        if self.attached_tool != VacuumTools.NONE:
+            self.get_logger().warn(f"Unable to attach {tool_type}. A tool is already attached")
+            return
+
+        p = self.tool_locations[tool_type]
+        above = Point(x=p.x, y=p.y, z=p.z+0.025)
+        attach = Point(x=p.x, y=p.y, z=p.z)
+        back = Point(x=p.x, y=p.y+0.05, z=p.z+0.025)
+
+        for i, position in enumerate([above, attach, above, back]):
+            plan = await self.plan_to_pose(Pose(position=position, orientation=self.orientations['tool_changer']), vsf=0.5, asf=0.2)
+            await self.execute(plan)
+
+            if i == 1:
+                await AsyncUtils.await_for_duration(self.get_clock(), Duration(seconds=0.5))
+                await self._attach_tool(tool_type)
+
+        self.attached_tool = tool_type
+
+    async def return_tool_to_stand(self):
+        if self.attached_tool == VacuumTools.NONE:
+            self.get_logger().warn(f"Unable to release. No tool currently attached")
+            return
+        
+        p = self.tool_locations[self.attached_tool]
+        above = Point(x=p.x, y=p.y, z=p.z+0.025)
+        detatch = Point(x=p.x, y=p.y, z=p.z)
+        back = Point(x=p.x, y=p.y+0.05, z=p.z+0.025)
+
+        for i, position in enumerate([back, above, detatch, above]):
+            plan = await self.plan_to_pose(Pose(position=position, orientation=self.orientations['tool_changer']), vsf=0.5, asf=0.2)
+            await self.execute(plan)
+
+            if i == 2:
+                await AsyncUtils.await_for_duration(self.get_clock(), Duration(seconds=0.5))
+                await self._detach_tool()
+
+        self.attached_tool = VacuumTools.NONE
+        
+    async def _attach_tool(self, tool: int):
+        await AsyncUtils.await_service_ready(self.attach_tool_client)
+
+        req = AttachTool.Request()
+        req.tool = tool
+
+        result = await AsyncUtils.await_service_response(self.attach_tool_client, req)
+
+        response = cast(AttachTool.Response, result)
+
+        if not response.success:
+            raise RuntimeError(response.message)
+        
+    async def _detach_tool(self):
+        await AsyncUtils.await_service_ready(self.detach_tool_client)
+
+        result = await AsyncUtils.await_service_response(self.detach_tool_client, Trigger.Request())
+
+        response = cast(Trigger.Response, result)
+
+        if not response.success:
+            raise RuntimeError(response.message)
+    
